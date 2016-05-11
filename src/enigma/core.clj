cec750d558f82c1e2b751946e7b88ec6d923ebbb
Jones Melton <shavah@gmail.com>
2016-05-11 15:44:54 -0500

    Initial commit

---
+++ src/enigma/core.clj
@@ -0,0 +1,7 @@
+(ns enigma.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/enigma/core_test.clj
@@ -0,0 +1,7 @@
+(ns enigma.core-test
+  (:require [clojure.test :refer :all]
+            [enigma.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
