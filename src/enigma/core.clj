650264bc896d9c1dca71ef580c415817a8216876
Jones Melton <shavah@gmail.com>
2016-05-17 10:36:47 -0500

    Add rotor functions

--- src/enigma/core.clj
+++ src/enigma/core.clj
@@ -1,10 +1,9 @@
 (ns enigma.core
   (:gen-class))
 
-(defn -main
 
   (def alphabet  "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-  
+
   (def rotor-1   "EKMFLGDQVZNTOWYHXUSPAIBRCJ")
   (def rotor-2   "AJDKSIRUXBLHWTMCQGZNPYFVOE")
   (def rotor-3   "BDFHJLCPRTXVZNYEIWGAKMUSQO")
@@ -12,5 +11,22 @@
   (def reflector-a "EJMZALYXVBWFCRQUONTSPIKHGD")
   (def reflector-b "YRUHQSLDPXNGOKMIEBFZCWVJAT")
   (def reflector-c "FVPJIAOYEDRZXWGCTKUQSBNMHL")
+
+  (defn rotor-into-wheel [rotor]
+    (seq rotor))
+
+  (defn rotate-wheel [wheel]
+    (concat (rest wheel) [(first wheel)]))
+
+  (defn index-of [char wheel]
+    (.indexOf wheel char))
+
+  (defn char-at [index wheel]
+    (nth wheel index))
+
+(defn -main
+  [& rest]
+  (def wheel-1 (rotor-into-wheel rotor-1))
+  (println (char-at 4 wheel-1))
 
 )
