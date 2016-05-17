984f2182aa0355e03000044d7a377221ba59caae
Eric Tenza <ejtenza@gmail.com>
2016-05-17 13:15:59 -0500

    add rotate-rotor functionality, update names (column-rotor => disc)

--- src/enigma/core.clj
+++ src/enigma/core.clj
@@ -4,19 +4,33 @@
 
   (def alphabet  "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-  (def rotor-1   "EKMFLGDQVZNTOWYHXUSPAIBRCJ")
-  (def rotor-2   "AJDKSIRUXBLHWTMCQGZNPYFVOE")
-  (def rotor-3   "BDFHJLCPRTXVZNYEIWGAKMUSQO")
+  (def disc-1   "EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+  (def disc-2   "AJDKSIRUXBLHWTMCQGZNPYFVOE")
+  (def disc-3   "BDFHJLCPRTXVZNYEIWGAKMUSQO")
 
   (def reflector-a "EJMZALYXVBWFCRQUONTSPIKHGD")
   (def reflector-b "YRUHQSLDPXNGOKMIEBFZCWVJAT")
   (def reflector-c "FVPJIAOYEDRZXWGCTKUQSBNMHL")
 
-  (defn rotor-into-wheel [rotor]
-    (seq rotor))
+  (defn disc-into-wheel [disc]
+    (seq disc))
 
   (defn rotate-wheel [wheel]
     (concat (rest wheel) [(first wheel)]))
+
+  (defn rotate-rotor [rotor]
+    {:alphabet (rotate-wheel (rotor :alphabet)),
+     :wheel (rotate-wheel (rotor :wheel)),
+     :notch (rotor :notch)})
+
+(rotate-rotor right-rotor)
+
+
+
+;;   (defn rotate-wheel [rotor]
+;;     (println (concat (rest (rotor :wheel)) [(first (rotor :wheel))]))
+;;     (println (concat (rest (rotor :alphabet)) [(first (rotor :alphabet))]))
+;;     "poop")
 
   (defn index-of [char wheel]
     (.indexOf wheel char))
@@ -26,7 +40,22 @@
 
 (defn -main
   [& rest]
-  (def wheel-1 (rotor-into-wheel rotor-1))
+  (def wheel-1 (disc-into-wheel rotor-1))
   (println (char-at 4 wheel-1))
 
 )
+
+(def right-rotor {:alphabet (disc-into-wheel alphabet),
+                :wheel (disc-into-wheel rotor-1),
+                :notch \V} )
+;; right-rotor
+
+;; (defn translate-char
+;;   [char rotor]
+;;   (if (= :notch (first :alphabet))
+;;     )
+;; )
+;; (translate-char \V )
+
+(= "poop" (rotate-wheel right-rotor))
+
