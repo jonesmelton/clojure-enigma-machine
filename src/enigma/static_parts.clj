d3324f4eff2415015c6eca438f773be24853e265
Eric Tenza <ejtenza@gmail.com>
2016-05-19 09:55:06 -0500

    add reflector-d, list of indices

--- src/enigma/static_parts.clj
+++ src/enigma/static_parts.clj
@@ -10,3 +10,5 @@
 (def reflector-a "EJMZALYXVBWFCRQUONTSPIKHGD")
 (def reflector-b "YRUHQSLDPXNGOKMIEBFZCWVJAT")
 (def reflector-c "FVPJIAOYEDRZXWGCTKUQSBNMHL")
+
+(def reflector-d '(24 17 20 7 16 18 11 3 15 23 13 6 14 10 12 8 4 1 5 25 2 22 21 9 0 19))
